23479515ce034182b17a388da812a3edd446241c
Harry <shaikmohammadibrahim311@gmail.com>
2024-09-08 07:17:56 +0530

    no message

--- ct-calculator-service/src/main/java/com/ct/calculator/ct_calculator_service/constans/EndPoint.java
+++ ct-calculator-service/src/main/java/com/ct/calculator/ct_calculator_service/constans/EndPoint.java
@@ -4,4 +4,5 @@ public class EndPoint {
 	private EndPoint() {}
 	public static final String CALC_MAPPING = "/calc";
 	public static final String ADD_MAPPING = "/add";
+	public static final String CALL_SERVICE = "/getAdd";
 }
