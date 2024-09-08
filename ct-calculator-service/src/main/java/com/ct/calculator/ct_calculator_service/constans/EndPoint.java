4716fdc9764dac7576fb2d95e86709e2c5033672
Mohammad Shaik <133103759+arbrf@users.noreply.github.com>
2024-09-08 07:09:30 +0530

    Add files via upload

---
+++ ct-calculator-service/src/main/java/com/ct/calculator/ct_calculator_service/constans/EndPoint.java
@@ -0,0 +1,7 @@
+package com.ct.calculator.ct_calculator_service.constans;
+
+public class EndPoint {
+	private EndPoint() {}
+	public static final String CALC_MAPPING = "/calc";
+	public static final String ADD_MAPPING = "/add";
+}
